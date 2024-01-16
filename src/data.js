64b43498f3cce3367533a9ded43bb1eaf7ece26a
alexa-whitney <alexawhitney@gmail.com>
2024-01-16 15:27:34 -0700

    Challenge 3 Completed (using set)

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
-import data from '../data.json';
+import data from './data.json';
 
 const allCategories = data.map(obj => obj.category);
 
-console.log(allCategories)
+// Make a set from an array - all values of the set will be unique!
+const categorieSet = new Set(allCategories) 
+// Make an array from a set with Array.from()
+const categoriesUnique = Array.from(categorieSet)
+
+console.log(categoriesUnique);
 
 export default data;
