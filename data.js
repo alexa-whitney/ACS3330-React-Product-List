eff3281cc5f55e809af896d1bceb4a90ec1b8898
alexa-whitney <alexawhitney@gmail.com>
2024-01-16 15:04:39 -0700

    Challenge 3 Completed

--- data.js
+++ data.js
@@ -2,5 +2,14 @@ import data from './data.json';
 
 const allCategories = data.map(obj => obj.category);
 
+// Make an object where each key is a category name
+const categoryObjects = allCategories.reduce((obj, cat) => {
+    obj[cat] = 0
+    return obj
+  }, {}) // !!! Be sure to define the initial value as an Object!
+  
+// Make an arr array of the keys
+const categoriesUnique = Object.keys(categoryObjects)
+
 export default data;
-export { allCategories };
+export { allCategories, categoriesUnique };
